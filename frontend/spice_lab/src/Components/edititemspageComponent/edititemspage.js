3c4ffed9572eeb846aaf489a8ad12784946a7c62
IT21061134 <nawanjanayeshmi@gmail.com>
2023-05-13 18:44:20 +0530

    create edit items page component

---
+++ frontend/spice_lab/src/Components/edititemspageComponent/edititemspage.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import "../edititemspageComponent/edititemspage.css";
+
+function EditItemsPage(){
+    return(
+        <div>
+            <div>
+                
+            </div>
+            <div>yashmi</div>
+        </div>
+    );
+}
+
+export default EditItemsPage;
