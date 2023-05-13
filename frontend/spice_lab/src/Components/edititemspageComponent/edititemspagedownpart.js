d0e10e5cec7fa723ee88e9645833f4b79d5df537
IT21061134 <nawanjanayeshmi@gmail.com>
2023-05-13 22:32:06 +0530

    update editpage  component

--- frontend/spice_lab/src/Components/edititemspageComponent/edititemspagedownpart.js
+++ frontend/spice_lab/src/Components/edititemspageComponent/edititemspagedownpart.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import "../edititemspageComponent/edititemspagedownpart.css";
 
+import itemimg from "../../images/nypic.png";
+import Button from '@mui/material/Button';
+
 
 
 function EditItemsPageDownPart(){
@@ -16,13 +19,43 @@ function EditItemsPageDownPart(){
                         <div className="col-sm-10 edititemspagedownpartA">
                             <div className="row">
                                 <div className="col-sm-4 makann">
-                                     
+                                    <img src={itemimg}  className="imgstyle"/>
                                 </div>
                                 <div className="col-sm-1 makann">
-                                    tttt
+                                    
                                 </div>
-                                <div className="col-sm-7 makann">
-                                    tttt
+                                <div className="col-sm-7 ">
+                                    <div className="row">
+                                        <div className="col-sm-4 editpagefont ">Full Name</div>
+                                        <div className="col-sm-8 ">
+                                            <input type="text" class="form-controlNY" placeholder=" example Mr.Lasitha Perera" required/>
+                                        </div>
+                                    </div>
+                                    <div className="row editpagespace">
+                                        <div className="col-sm-4 editpagefont ">Weight</div>
+                                        <div className="col-sm-8 ">
+                                            <input type="text" class="form-controlNY" placeholder=" example KG" required/>
+                                        </div>
+                                    </div>
+                                    <div className="row editpagespace">
+                                        <div className="col-sm-4 editpagefont">Price</div>
+                                        <div className="col-sm-8 ">
+                                            <input type="text" class="form-controlNY" placeholder=" example $100" required/>
+                                        </div>
+                                    </div>
+                                    <div className="row editpagespace">
+                                        <div className="col-sm-12 editpagefont ">Description</div>
+                                        <textarea className="form-controlNY editpagespaceA" placeholder=" example (Pvt) Ltd" required></textarea>  
+                                    </div>
+                                    <div className="row editpagespace">
+                                        <div className="col-sm-6"></div>
+                                        <div className="col-sm-3 ">
+                                            <center><Button variant="contained" style={{backgroundColor: '#4D1003', color: 'white', width: '100px'}}>Cancel</Button></center>
+                                        </div> 
+                                        <div className="col-sm-3">
+                                            <center><Button variant="contained" style={{backgroundColor: '#4D1003', color: 'white', width: '100px'}}>Save</Button></center>
+                                        </div> 
+                                    </div>
                                 </div>
                             </div>
                         </div>
