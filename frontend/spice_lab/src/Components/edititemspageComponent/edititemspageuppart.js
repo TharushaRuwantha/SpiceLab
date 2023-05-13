312d98b9c200b9f2667ad5c265602688c184520b
IT21061134 <nawanjanayeshmi@gmail.com>
2023-05-13 19:15:58 +0530

    create edit items page up components (topic part)

--- frontend/spice_lab/src/Components/edititemspageComponent/edititemspageuppart.js
+++ frontend/spice_lab/src/Components/edititemspageComponent/edititemspageuppart.js
@@ -3,7 +3,17 @@ import "../edititemspageComponent/edititemspageuppart.css";
 
 function EditItemsPageUpPart(){
     return(
-        <div>thrusha</div>
+        <div>
+            <div className="row ">
+                <div className="col-sm-1"></div>
+
+                <div className="col-sm-10 edititemspage">
+                    <div className="edititemspagetopic">Add New Items</div>
+                </div>
+
+                <div className="col-sm-1"></div>  
+            </div>
+        </div>
     );
 }
 
