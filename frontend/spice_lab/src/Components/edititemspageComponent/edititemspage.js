fdf10a221ffb85be535f70525b7d06d0b4175eaf
IT21061134 <nawanjanayeshmi@gmail.com>
2023-05-13 18:51:47 +0530

    create EditItemsPageUpPart componet

--- frontend/spice_lab/src/Components/edititemspageComponent/edititemspage.js
+++ frontend/spice_lab/src/Components/edititemspageComponent/edititemspage.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import "../edititemspageComponent/edititemspage.css";
 
+import EditItemsPageUpPart from "./edititemspageuppart";
+
 function EditItemsPage(){
     return(
         <div>
             <div>
-                
+                <EditItemsPageUpPart/>
             </div>
             <div>yashmi</div>
         </div>
---
+++ frontend/spice_lab/src/Components/edititemspageComponent/edititemspageuppart.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import "../edititemspageComponent/edititemspageuppart.css";
+
+function EditItemsPageUpPart(){
+    return(
+        <div>thrusha</div>
+    );
+}
+
+export default EditItemsPageUpPart;
