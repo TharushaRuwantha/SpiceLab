1babe13cf24039da3fbe3df5a68f5a0c99672819
IT21061134 <nawanjanayeshmi@gmail.com>
2023-05-14 17:25:39 +0530

    fully created products summary page

--- frontend/spice_lab/src/Components/productssummarypageComponent/productssummarypagedownpart.js
+++ frontend/spice_lab/src/Components/productssummarypageComponent/productssummarypagedownpart.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import "../productssummarypageComponent/productssummarypagedownpart.css";
+import Button from '@mui/material/Button';
+import Pagination from '@mui/material/Pagination';
 
 function ProductsSummaryPageDownPart(){
     return(
@@ -40,14 +42,7 @@ function ProductsSummaryPageDownPart(){
                         <div className="col-sm-3 tableborderA"><center className="productssummarypagetextA ">5000KG</center></div>
                         <div className="col-sm-3 tableborderA"><center className="productssummarypagetextA ">Rs.15 000</center></div>
                     </div>
-                    <br></br>
-                    <div className="row productssummarypagedownpartcolour">
-                        <div className="col-sm-1 tableborderA"><center className="productssummarypagetextA " ><u>View</u></center></div>
-                        <div className="col-sm-2 tableborderA"><center className="productssummarypagetextA ">E001-11</center></div>
-                        <div className="col-sm-3 tableborderA"><center className="productssummarypagetextA ">Pepper Powder</center></div>
-                        <div className="col-sm-3 tableborderA"><center className="productssummarypagetextA ">5000KG</center></div>
-                        <div className="col-sm-3 tableborderA"><center className="productssummarypagetextA ">Rs.15 000</center></div>
-                    </div>
+                    
                     <br></br>
                     <div className="row productssummarypagedownpartcolour">
                         <div className="col-sm-1 tableborderA"><center className="productssummarypagetextA " ><u>View</u></center></div>
@@ -71,8 +66,39 @@ function ProductsSummaryPageDownPart(){
                 <div className="col-sm-1"></div>
 
             </div>
-            <div>
-                hjsdjs
+
+            <div className="row">
+                <div className="col-sm-1"></div>
+                <div className="col-sm-10">
+                    <div className="row productsammeryunderpartA ">
+
+                        <div className="col-sm-10"></div>
+                        <div className="col-sm-2 ">
+                            <center>
+                            <Button variant="contained" style={{backgroundColor: '#4D1003', color: 'white', width: '100%'}}>Print</Button>
+                            </center>
+                        </div> 
+
+                    </div>
+                </div>
+                <div className="col-sm-1"></div>  
+            </div>
+
+            <div className="row">
+                <div className="col-sm-1"></div>
+                <div className="col-sm-10">
+                    <div className="row productsammeryunderpartB ">
+
+                        <div className="col-sm-9"></div>
+                        <div className="col-sm-3 ">
+                            <center>
+                                <Pagination count={4} variant="outlined" shape="rounded" />       
+                            </center>
+                        </div> 
+
+                    </div>
+                </div>
+                <div className="col-sm-1"></div>  
             </div>
         </div>
     );
