fa0c70bd51bebd7ce24ec35bff3c4bfc197cc2db
IT21061134 <nawanjanayeshmi@gmail.com>
2023-05-15 16:07:27 +0530

    update profile edit left side page

--- frontend/spice_lab/src/Components/profileeditComponent/profileeditpageleftside.js
+++ frontend/spice_lab/src/Components/profileeditComponent/profileeditpageleftside.js
@@ -1,16 +1,57 @@
 import React,{useState} from "react";
 import "../profileeditComponent/profileeditpageleftside.css";
+import NYprofile from "../../images/NYprofile.png"
 
 function ProfileEditLeftSide(){
     return(
         <div>
-            <div className="row">
+            <div className="row profileeditmain">
 
-                <div className="col-sm-1 makanna">jdjd</div>
-                <div className="col-sm-3 makanna">jsjjs</div>
-                <div className="col-sm-1 makanna">kaka</div>
-                <div className="col-sm-6 makanna">lala</div>
-                <div className="col-sm-1">kakka</div>
+                <div className="col-sm-1 "></div>
+                <div className="col-sm-3  profileeditmainleft">
+
+                    <div className="row">
+                        <div className="col-sm-1"></div>
+                        < div className="col-sm-10">
+                           
+                            <div className="row">
+                                <center>
+                                    <img className="profilepageimg" src={NYprofile}/>
+                                </center>
+                            </div>
+
+                            <div className="row">
+                                <center>
+                                    <div className="profilemainnametext">
+                                        Shanika Dilushani
+                                    </div>
+                                    <div className="profilemainemailtext">
+                                        dilushani@gmail.com
+                                    </div>
+                                </center>
+                            </div>
+
+                            <div className="row profile_description_topic_space">
+                                <center>
+                                    <div className="profile_description_topic">
+                                        About
+                                    </div>
+                                    <div className="profile_description">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc m nec erat dolor sit amet, consectetur adipiscing elit. Nunc m nec erat dolor sit amet, consectetur adipiscing elit. Nunc m nec erat
+                                    </div>
+                                </center>
+                            </div>
+
+                            <div className="row underprofile"></div>
+                            
+                        </div>
+                        <div className="col-sm-1"></div>
+                    </div>
+
+                </div>
+                <div className="col-sm-1 makanna"></div>
+                <div className="col-sm-6 makanna"></div>
+                <div className="col-sm-1 makanna"></div>
 
             </div>
         </div>
