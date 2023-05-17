80c642f342c9fcca3227d13fad441e19f15b3eef
Yasithri Chamathka Jinasena <it21041266@my.sliit.lk>
2023-05-17 09:06:29 +0530

    updateproducthome

--- frontend/spice_lab/src/Components/ProductHomeComponent/ProductHome.js
+++ frontend/spice_lab/src/Components/ProductHomeComponent/ProductHome.js
@@ -1,89 +1,45 @@
-import './ProductHome.css';
 import React from "react";
 import Grid from '@mui/material/Grid';
 import Cinnamon_image from '../../images/cinnamon.jpeg';
 import tamarind_image from '../../images/tamarind.png';
 import chili_image from '../../images/chili.jpg';
+import nutmeg_image from '../../images/nutmeg.png';
+import turmeric_image from '../../images/turmeric.png';
+import saffron_image from '../../images/saffron.png';
 
+import './ProductHome.css';
 
-
-// meka wens kara ae guthub add krnn oni
-
-function ProductHome(){
-
-    return(
-        <Grid container>
-                    <Grid item x5={3}>
-                   
-                    
-                    <div class="card text-bg-dark">
-                            <img src={Cinnamon_image} class="card-img"/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Sweet Spices</h5>
-                                 
-                            </div>
-                    </div> 
-
-                    <div class="card text-bg-dark">
-                            <img src={Cinnamon_image} class="card-img"/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Sour Spices</h5>
-                                 
-                            </div>
-                        </div>     
-
-
-
-
-                    </Grid>
-                    <Grid item x5={6}>
-                    <div class="card text-bg-dark">
-                            <img src={Cinnamon_image} class="card-img"/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Hot Spices</h5>
-                                 
-                            </div>
-                        </div> 
-
-                        <div class="card text-bg-dark">
-                            <img src={Cinnamon_image} class="card-img"/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Aromatic Spices</h5>
-                                 
-                            </div>
-                        </div> 
-
-                    </Grid>
-
-
-                    
-                    <Grid item x5={3}>
-                    <div class="card text-bg-dark">
-                            <img src={Cinnamon_image} class="card-img"/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Exotic Spices</h5>
-                                
-                                
-                            </div>
-                        </div> 
-
-                        <div class="card text-bg-dark">
-                            <img src={Cinnamon_image} class="card-img"/>
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Card title</h5>
-                                 
-                            </div>
-                        </div> 
-
-                    </Grid>
-
-        </Grid>
-        
-
-    )
-        
-
+function ProductHome() {
+  return (
+    <div className="image-container">
+      <div className="card-container">
+        <div className="card">
+          <h5 className="card-title">Sweet Spices</h5>
+          <img src={Cinnamon_image} alt="Cinnamon" className="card-img" />
+        </div>
+        <div className="card">
+          <h5 className="card-title">Sour Spices</h5>
+          <img src={tamarind_image} alt="Tamarind" className="card-img" />
+        </div>
+        <div className="card">
+          <h5 className="card-title">Hot Spices</h5>
+          <img src={chili_image} alt="Chili" className="card-img" />
+        </div>
+        <div className="card">
+          <h5 className="card-title">Aromatic Spices</h5>
+          <img src={nutmeg_image} alt="Nutmeg" className="card-img" />
+        </div>
+        <div className="card">
+          <h5 className="card-title">Exotic Spices</h5>
+          <img src={turmeric_image} alt="Turmeric" className="card-img" />
+        </div>
+        <div className="card">
+          <h5 className="card-title">Card title</h5>
+          <img src={saffron_image} alt="Saffron" className="card-img" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductHome;
-
